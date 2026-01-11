220fc579b22abf86d6d9b365b719bcf8d03e2225
Bruce <bruce@moerdjiman.com>
2026-01-11 16:59:33 +0100

    added swup for page transitions , updated home

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import swup from '@swup/astro';
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    swup({ theme: false })
+  ]
+
 });
