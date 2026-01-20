21c49917c8246b1b83680c8478b41ca0171bff12
Bruce <bruce@moerdjiman.com>
2026-01-20 20:48:08 +0100

    new website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import swup from '@swup/astro';
 
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  },
-  integrations: [
-    swup({ theme: false })
-  ]
+    plugins: [tailwindcss()],
+  }
 
 });
